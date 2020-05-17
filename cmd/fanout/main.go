4ad72ab4c6bb96efc3cd7aefb44a858a8036c7e6
Montgomery Watts <mjw4633@rit.edu>
2020-05-17 18:50:52 -0400

    go routines. Ignore compiled code

--- cmd/fanout/main.go
+++ cmd/fanout/main.go
@@ -15,26 +15,42 @@ import (
 func handler(evt events.SQSEvent) {
 	client := sp.NewSpotifyClient()
 	var publisher publisher.Publisher = sns.NewSNSPublisher()
+	outerChan := make(chan bool)
+	numSignals := 0
 
 	for _, msg := range evt.Records {
 		msgAttrs := msg.MessageAttributes
 		ID := msg.Body
 		switch entityType := *(msgAttrs["entity_type"].StringValue); entityType {
 		case "artist":
-			albums, err := client.GetArtistAlbums(spotify.ID(ID))
-			if err != nil {
-				log.Fatalf("Unable to get albums for artist with ID %s", ID)
-			}
-			for ok := true; ok; ok = (albums.Next != "") {
-				for _, album := range albums.Albums {
-					albumID := album.ID.String()
-					publisher.PublishAlbumID(albumID)
+			numSignals++
+			go func() {
+				albums, err := client.GetArtistAlbums(spotify.ID(ID))
+				if err != nil {
+					log.Fatalf("Unable to get albums for artist with ID %s", ID)
 				}
-				client.NextPage(albums)
-			}
+				innerChan := make(chan error, albums.Total)
+				for ok := true; ok; ok = (albums.Next != "") {
+					for _, album := range albums.Albums {
+						albumID := album.ID.String()
+						go func() {
+							innerChan <- publisher.PublishAlbumID(albumID)
+						}()
+					}
+					client.NextPage(albums)
+				}
+				for i := 0; i < albums.Total; i++ {
+					<-innerChan
+				}
+				outerChan <- true
+			}()
 		default:
 			log.Fatalf("Unknown entity type received: %s", entityType)
 		}
+	}
+
+	for signals := 0; signals < numSignals; signals++ {
+		<-outerChan
 	}
 }
 
